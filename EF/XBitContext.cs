18c0af33b5092f87840c95136a023e1686ac5c93
Ofaso <kevinwuestner5@gmail.com>
2018-05-11 15:03:50 +0200

    initial commit

---
+++ EF/XBitContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace XBitApi.EF
+{
+    public class XBitContext : DbContext
+    {
+        public XBitContext(DbContextOptions<XBitContext> context) : base(context) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=XBit;Integrated Security=SSPI");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EF/XBitContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace XBitApi.EF
+{
+    public class XBitContextFactory : IDesignTimeDbContextFactory<XBitContext>
+    {
+        public XBitContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<XBitContext>();
+            return new XBitContext(optionsBuilder.Options);
+        }
+    }
+}
