f2e680160768479413aad78d45ac1e4146fa607e
Ofaso <kevinwuestner5@gmail.com>
2018-05-15 18:57:00 +0200

    feat(controller): add address and countrycontroller

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using XBitApi.Models;
+using XBitApi.EF;
+
+namespace XBitApi.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class AddressController : Controller
+    {
+        private XBitContext context;
+
+        public AddressController(XBitContext context)
+        {
+            this.context = context;
+        }
+
+        // GET api/address?countryId=test
+        [HttpGet]
+        public IActionResult GetAddresses()
+        {
+            return Ok(context.Addresses.ToList());
+        }
+
+        // GET api/address/Guid
+        [HttpGet("{id}")]
+        public IActionResult GetAddress(Guid id)
+        {
+            Address address = context.Addresses.Find(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return Ok(address);
+        }
+
+        // POST api/address
+        [HttpPost]
+        public IActionResult PostAddress([FromBody]Address address)
+        {
+            context.Addresses.Add(address);
+            context.SaveChanges();
+            return CreatedAtAction(address.Id.ToString(), address);
+        }
+
+        // PUT api/address
+        [HttpPut]
+        public IActionResult PutAddress([FromBody]Address address)
+        {
+            context.Addresses.Update(address);
+            context.SaveChanges();
+            return Ok(address);
+        }
+
+        // DELETE api/address/Guid
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAddress(Guid id)
+        {
+            context.Addresses.Remove(context.Addresses.Find(id));
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/CountryController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using XBitApi.Models;
+using XBitApi.EF;
+
+namespace XBitApi.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        private XBitContext context;
+
+        public CountryController(XBitContext context)
+        {
+            this.context = context;
+        }
+
+        // GET api/country
+        [HttpGet]
+        public IActionResult GetCountries()
+        {
+            return Ok(context.Countries.ToList());
+        }
+
+        // GET api/country/Guid
+        [HttpGet("{id}")]
+        public IActionResult GetCountry(Guid id)
+        {
+            Country country = context.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        // POST api/country
+        [HttpPost]
+        public IActionResult PostCountry([FromBody]Country country)
+        {
+            context.Countries.Add(country);
+            context.SaveChanges();
+            return CreatedAtAction(country.Id.ToString(), country);
+        }
+
+        // PUT api/country
+        [HttpPut]
+        public IActionResult PutCountry([FromBody]Country country)
+        {
+            context.Countries.Update(country);
+            context.SaveChanges();
+            return Ok(country);
+        }
+
+        // DELETE api/country/id
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCountry(Guid id)
+        {
+            context.Countries.Remove(context.Countries.Find(id));
+            return Ok();
+        }
+    }
+}
