28e67254dde8f7094109e8f8a837104b6629653a
Ofaso <kevinwuestner5@gmail.com>
2018-05-17 21:03:33 +0200

    refactor(models): add migration to model change

--- Migrations/20180517184458_ChangeModels.Designer.cs
+++ Migrations/20180517184458_ChangeModels.Designer.cs
@@ -11,9 +11,10 @@ using XBitApi.EF;
 namespace XBitApi.Migrations
 {
     [DbContext(typeof(XBitContext))]
-    partial class XBitContextModelSnapshot : ModelSnapshot
+    [Migration("20180517184458_ChangeModels")]
+    partial class ChangeModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
