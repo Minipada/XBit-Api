1963441539d7f41ff14a7927880fa50e62b2362d
Mario Zi <mario.gwerder@gmail.com>
2018-05-22 19:29:03 +0200

    brr zi

---
+++ LoggingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XBitApi.EF;
+
+namespace XBitApi
+{
+    public class LoggingService
+    {
+        private XBitContext context;
+        public LoggingService(XBitContext context)
+        {
+            this.context = context;
+        }
+
+        public void LogError()
+        {
+
+        }
+
+
+        public void LogInfo()
+        {
+
+        }
+
+        public void LogWarning()
+        {
+
+        }
+    }
+}
